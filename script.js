3c04a63b0145416bdf639fcc9039350d8483a996
gstifla007 <124055358+gstifla007@users.noreply.github.com>
2023-06-21 15:37:03 -0600

    added input limit enforce to date inputs

--- script.js
+++ script.js
@@ -6,9 +6,29 @@ window.addEventListener('DOMContentLoaded', () => {
   const monthResult = document.getElementById('mmResult');
   const dayResult = document.getElementById('yyResult');
 
+  const enforceTwoDigitInput = (input) => {
+    if (input.value.length > 2) {
+      input.value = input.value.slice(0, 2);
+    }
+  };
+
+  const enforceFourDigitInput = (input) => {
+    if (input.value.length > 4) {
+      input.value = input.value.slice(0, 4);
+    }
+  };
+
   const calculateAge = () => {
     const today = new Date();
     const birthDate = new Date(`${monthInput.value}/${dayInput.value}/${yearInput.value}`);
+
+    if (isNaN(birthDate)) {
+      // Display "--" if the birth date is invalid
+      yearResult.textContent = '--';
+      monthResult.textContent = '--';
+      dayResult.textContent = '--';
+      return;
+    }
 
     const yearDiff = today.getFullYear() - birthDate.getFullYear();
     const monthDiff = today.getMonth() - birthDate.getMonth();
@@ -28,9 +48,10 @@ window.addEventListener('DOMContentLoaded', () => {
       monthDiff--;
     }
 
-    yearResult.textContent = yearDiff;
-    monthResult.textContent = monthDiff;
-    dayResult.textContent = dayDiff;
+    // Display "--" if the age is 0
+    yearResult.textContent = yearDiff === 0 ? '--' : yearDiff;
+    monthResult.textContent = monthDiff === 0 ? '--' : monthDiff;
+    dayResult.textContent = dayDiff === 0 ? '--' : dayDiff;
   };
 
   const form = document.getElementById('ageCalculatorForm');
@@ -39,4 +60,18 @@ window.addEventListener('DOMContentLoaded', () => {
     e.preventDefault();
     calculateAge();
   });
+
+  dayInput.addEventListener('input', () => {
+    enforceTwoDigitInput(dayInput);
+  });
+
+  monthInput.addEventListener('input', () => {
+    enforceTwoDigitInput(monthInput);
+  });
+
+  yearInput.addEventListener('input', () => {
+    enforceFourDigitInput(yearInput);
+  });
+
+  calculateAge(); // Calculate age initially
 });
