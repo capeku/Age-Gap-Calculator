d34db686ffde44b51c600e658b8f37f59bf68e5c
gstifla007 <124055358+gstifla007@users.noreply.github.com>
2023-06-22 09:16:22 -0600

    Automated Age calculation
    Removed button and added event handlers to check for input and calculate age

--- script.js
+++ script.js
@@ -18,112 +18,109 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   const calculateAge = () => {
-  const today = new Date();
-  const birthYear = parseInt(yearInput.value, 10);
-  const birthMonth = parseInt(monthInput.value, 10);
-  const birthDay = parseInt(dayInput.value, 10);
+    const today = new Date();
+    const birthYear = parseInt(yearInput.value, 10);
+    const birthMonth = parseInt(monthInput.value, 10);
+    const birthDay = parseInt(dayInput.value, 10);
 
-  let hasError = false;
+    let hasError = false;
 
-  // Reset error messages
-  dayError.textContent = '';
-  monthError.textContent = '';
-  yearError.textContent = '';
+    // Reset error messages
+    dayError.textContent = '';
+    monthError.textContent = '';
+    yearError.textContent = '';
 
-  if (isNaN(birthYear) || yearInput.value.length !== 4 || birthYear >= today.getFullYear()) {
-    if (yearInput.value === '') {
-      yearError.textContent = 'This field is empty';
-    } else {
-      yearError.textContent = 'Must be in the past';
-    }
-    hasError = true;
-  }
-
-  if (isNaN(birthMonth) || birthMonth < 1 || birthMonth > 12) {
-    if (monthInput.value === '') {
-      monthError.textContent = 'This field is empty';
-    } else {
-      monthError.textContent = 'Must be a valid month';
-    }
-    hasError = true;
-  }
-
-  if (isNaN(birthDay) || birthDay < 1 || birthDay > 31) {
-    if (dayInput.value === '') {
-      dayError.textContent = 'This field is empty';
-    } else {
-      dayError.textContent = 'Must be a valid day';
-    }
-    hasError = true;
-  } else {
-    // Check for invalid dates (e.g., 31/04/1991)
-    const lastDayOfMonth = new Date(birthYear, birthMonth, 0).getDate();
-    if (birthDay > lastDayOfMonth) {
-      dayError.textContent = 'Invalid date';
+    if (isNaN(birthYear) || yearInput.value.length !== 4 || birthYear >= today.getFullYear()) {
+      if (yearInput.value === '') {
+        yearError.textContent = 'This field is empty';
+      } else {
+        yearError.textContent = 'Must be in the past';
+      }
       hasError = true;
     }
-  }
 
-  if (hasError) {
-    // Display "--" in the result fields
-    yearResult.textContent = '--';
-    monthResult.textContent = '--';
-    dayResult.textContent = '--';
-    return;
-  }
+    if (isNaN(birthMonth) || birthMonth < 1 || birthMonth > 12) {
+      if (monthInput.value === '') {
+        monthError.textContent = 'This field is empty';
+      } else {
+        monthError.textContent = 'Must be a valid month';
+      }
+      hasError = true;
+    }
 
-  const birthDate = new Date(birthYear, birthMonth - 1, birthDay);
-  if (birthDate > today) {
-    // Display "--" if the birth date is in the future
-    yearResult.textContent = '--';
-    monthResult.textContent = '--';
-    dayResult.textContent = '--';
-    return;
-  }
+    if (isNaN(birthDay) || birthDay < 1 || birthDay > 31) {
+      if (dayInput.value === '') {
+        dayError.textContent = 'This field is empty';
+      } else {
+        dayError.textContent = 'Must be a valid day';
+      }
+      hasError = true;
+    } else {
+      // Check for invalid dates (e.g., 31/04/1991)
+      const lastDayOfMonth = new Date(birthYear, birthMonth, 0).getDate();
+      if (birthDay > lastDayOfMonth) {
+        dayError.textContent = 'Invalid date';
+        hasError = true;
+      }
+    }
 
-  let yearDiff = today.getFullYear() - birthYear;
-  let monthDiff = today.getMonth() - birthMonth;
-  let dayDiff = today.getDate() - birthDay;
+    if (hasError) {
+      // Display "--" in the result fields
+      yearResult.textContent = '--';
+      monthResult.textContent = '--';
+      dayResult.textContent = '--';
+      return;
+    }
 
-  if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
-    yearDiff--;
-  }
+    const birthDate = new Date(birthYear, birthMonth - 1, birthDay);
+    if (birthDate > today) {
+      // Display "--" if the birth date is in the future
+      yearResult.textContent = '--';
+      monthResult.textContent = '--';
+      dayResult.textContent = '--';
+      return;
+    }
 
-  if (monthDiff < 0) {
-    monthDiff += 12;
-  }
+    let yearDiff = today.getFullYear() - birthYear;
+    let monthDiff = today.getMonth() - birthMonth;
+    let dayDiff = today.getDate() - birthDay;
 
-  if (dayDiff < 0) {
-    const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
-    dayDiff += lastDayOfMonth;
-    monthDiff--;
-  }
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+      yearDiff--;
+    }
 
-  // Display "--" if the age is 0
-  yearResult.textContent = yearDiff === 0 ? '--' : yearDiff;
-  monthResult.textContent = monthDiff === 0 ? '--' : monthDiff;
-  dayResult.textContent = dayDiff === 0 ? '--' : dayDiff;
-};
+    if (monthDiff < 0) {
+      monthDiff += 12;
+    }
 
+    if (dayDiff < 0) {
+      const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+      dayDiff += lastDayOfMonth;
+      monthDiff--;
+    }
+
+    // Display "--" if the age is 0
+    yearResult.textContent = yearDiff === 0 ? '--' : yearDiff;
+    monthResult.textContent = monthDiff === 0 ? '--' : monthDiff;
+    dayResult.textContent = dayDiff === 0 ? '--' : dayDiff;
+  };
 
   const form = document.getElementById('ageCalculatorForm');
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    calculateAge();
-  });
-
   dayInput.addEventListener('input', () => {
     enforceTwoDigitInput(dayInput);
+    calculateAge();
   });
 
   monthInput.addEventListener('input', () => {
     enforceTwoDigitInput(monthInput);
+    calculateAge();
   });
 
   yearInput.addEventListener('input', () => {
     enforceFourDigitInput(yearInput);
+    calculateAge();
   });
 
-  calculateAge(); // Calculate age initially
+  //calculateAge(); // Calculate age initially
 });
