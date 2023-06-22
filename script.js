422d82ba795139fc0a398d0dbcff27b15f4620ff
gstifla007 <124055358+gstifla007@users.noreply.github.com>
2023-06-21 19:28:48 -0600

    Date calculator now works

--- script.js
+++ script.js
@@ -7,62 +7,64 @@ window.addEventListener('DOMContentLoaded', () => {
   const dayResult = document.getElementById('yyResult');
 
   const enforceTwoDigitInput = (input) => {
-    if (input.value.length > 2) {
-      input.value = input.value.slice(0, 2);
-    }
+    input.value = input.value.slice(0, 2); // Limit input to two digits
   };
 
   const enforceFourDigitInput = (input) => {
-    if (input.value.length > 4) {
-      input.value = input.value.slice(0, 4);
-    }
+    input.value = input.value.slice(0, 4); // Limit input to four digits
   };
 
-const calculateAge = () => {
-  const today = new Date();
-  const birthYear = parseInt(yearInput.value, 10);
+  const calculateAge = () => {
+    const today = new Date();
+    const birthYear = parseInt(yearInput.value, 10);
+    const birthMonth = parseInt(monthInput.value, 10);
+    const birthDay = parseInt(dayInput.value, 10);
 
-  if (birthYear.toString().padStart(4, '0') !== yearInput.value) {
-    // Display "--" if the birth date is invalid
-    yearResult.textContent = '--';
-    monthResult.textContent = '--';
-    dayResult.textContent = '--';
-    return;
-  }
+    if (
+      isNaN(birthYear) ||
+      isNaN(birthMonth) || birthMonth < 1 || birthMonth > 12 ||
+      isNaN(birthDay) || birthDay < 1 || birthDay > 31 ||
+      yearInput.value.length !== 4 || birthYear >= today.getFullYear()
+    ) {
+      // Display "--" if the birth date is invalid
+      yearResult.textContent = '--';
+      monthResult.textContent = '--';
+      dayResult.textContent = '--';
+      return;
+    }
 
-  const birthDate = new Date(`${monthInput.value}/${dayInput.value}/${birthYear}`);
+    const birthDate = new Date(birthYear, birthMonth - 1, birthDay);
+    if (birthDate > today) {
+      // Display "--" if the birth date is in the future
+      yearResult.textContent = '--';
+      monthResult.textContent = '--';
+      dayResult.textContent = '--';
+      return;
+    }
 
-  if (isNaN(birthDate)) {
-    // Display "--" if the birth date is invalid
-    yearResult.textContent = '--';
-    monthResult.textContent = '--';
-    dayResult.textContent = '--';
-    return;
-  }
+    let yearDiff = today.getFullYear() - birthYear;
+    let monthDiff = today.getMonth() - birthMonth;
+    let dayDiff = today.getDate() - birthDay;
 
-  const yearDiff = today.getFullYear() - birthDate.getFullYear();
-  const monthDiff = today.getMonth() - birthDate.getMonth();
-  const dayDiff = today.getDate() - birthDate.getDate();
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+      yearDiff--;
+    }
 
-  if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
-    yearDiff--;
-  }
+    if (monthDiff < 0) {
+      monthDiff += 12;
+    }
 
-  if (monthDiff < 0) {
-    monthDiff += 12;
-  }
+    if (dayDiff < 0) {
+      const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+      dayDiff += lastDayOfMonth;
+      monthDiff--;
+    }
 
-  if (dayDiff < 0) {
-    const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
-    dayDiff += lastDayOfMonth;
-    monthDiff--;
-  }
-
-  // Display "--" if the age is 0
-  yearResult.textContent = yearDiff === 0 ? '--' : yearDiff;
-  monthResult.textContent = monthDiff === 0 ? '--' : monthDiff;
-  dayResult.textContent = dayDiff === 0 ? '--' : dayDiff;
-};
+    // Display "--" if the age is 0
+    yearResult.textContent = yearDiff === 0 ? '--' : yearDiff;
+    monthResult.textContent = monthDiff === 0 ? '--' : monthDiff;
+    dayResult.textContent = dayDiff === 0 ? '--' : dayDiff;
+  };
 
   const form = document.getElementById('ageCalculatorForm');
 
